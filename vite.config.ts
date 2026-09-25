58fa14067dbc1ac2774fdbc45d225ca323b2d2a6
Hung Vu <29vu07gia07hung@gmail.com>
2026-09-25 18:47:26 +0700

    feat: configure dev server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve("./src"),
